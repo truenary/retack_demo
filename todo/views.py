4ca806f26c37cea9b09292dd3493551fc8bb12db
trunary.com <contact@truenary.com>
2024-06-04 06:37:24 +0000

    fix: Error in index: todo/index1.html

--- todo/views.py
+++ todo/views.py
@@ -14,7 +14,7 @@ def index(request):
         todo_list = Todo.objects.order_by('id')
         form = TodoForm()
         context = {'todo_list': todo_list, 'form': form}
-        return render(request, 'todo/index.html', context)
+        return render(request, 'todo/index1.html', context)
     except Exception as e:
         report_error(e, "index", request)
 
